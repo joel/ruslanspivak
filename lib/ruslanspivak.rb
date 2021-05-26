052b5dabdccf7ab8337e8372f69192a1d43bd7cb
Joel Azemar <joel.azemar@gmail.com>
2021-05-26 16:58:40 +0200

    Initial Commit

---
+++ lib/ruslanspivak.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ruslanspivak/version"
+
+module Ruslanspivak
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/ruslanspivak_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Ruslanspivak do
+  it "has a version number" do
+    expect(Ruslanspivak::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
