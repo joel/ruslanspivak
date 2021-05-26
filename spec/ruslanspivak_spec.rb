8924a192f91abdce6ec644ecad15ff50918dc46c
Joel Azemar <joel.azemar@gmail.com>
2021-05-26 16:58:58 +0200

    Change Spec

--- spec/ruslanspivak_spec.rb
+++ spec/ruslanspivak_spec.rb
@@ -1,11 +1,1 @@
-# frozen_string_literal: true
-
-RSpec.describe Ruslanspivak do
-  it "has a version number" do
-    expect(Ruslanspivak::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
+RSpec.describe Ruslanspivak
