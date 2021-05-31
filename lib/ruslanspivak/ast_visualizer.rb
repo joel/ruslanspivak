1f26ee5d1d37b5cc11045ae06e8a0f707d90c56a
Joel Azemar <joel.azemar@gmail.com>
2021-05-31 17:47:09 +0200

    Update ast_visualizer.rb

--- lib/ruslanspivak/ast_visualizer.rb
+++ lib/ruslanspivak/ast_visualizer.rb
@@ -18,43 +18,41 @@ module Ruslanspivak
     end
 
     def visit_num(node)
-      s = "  node#{@ncount} [label=\"#{node.token.value}\"]"
-      @dot_body << s
-      node.num = @ncount
-      @ncount += 1
+      add_line(node: node) { "node#{@ncount} [label=\"#{node.value}\"]" }
     end
 
     def visit_bin_op(node)
-      s = "  node#{@ncount} [label=\"#{node.operator.value}\"]"
-      @dot_body << s
-      node.num = @ncount
-      @ncount += 1
+      add_line(node: node) { "node#{@ncount} [label=\"#{node.operator.value}\"]" }
 
       visit(node.left)
       visit(node.right)
 
       [node.left, node.right].each do |child_node|
-        # puts("[#{node}][#{node.num}] => [#{child_node}][#{child_node.num}]")
-        s = "  node#{node.num} -> node#{child_node.num}"
-        @dot_body << s
+        add_line(node: node, go_forward: false) { "node#{node.num} -> node#{child_node.num}" }
       end
     end
 
     def visit_unary_op(node)
-      s = "  node#{@ncount} [label=\"unary #{node.operator.value}\"]"
-      @dot_body << s
-      node.num = @ncount
-      @ncount += 1
+      add_line(node: node) { "node#{@ncount} [label=\"unary #{node.operator.value}\"]" }
 
       visit(node.expr)
-      s = "  node#{node.num} -> node#{node.expr.num}"
-      @dot_body << s
+      add_line(node: node, go_forward: false) { "node#{node.num} -> node#{node.expr.num}" }
     end
 
     def gendot
       tree = @parser.parse
       visit(tree)
       (@dot_header + @dot_body + @dot_footer).join("\n")
+    end
+
+    private
+
+    def add_line(node:, go_forward: true)
+      @dot_body << "  #{yield}"
+      return unless go_forward
+
+      node.num = @ncount
+      @ncount += 1
     end
   end
 end
